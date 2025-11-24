c844f9224b9755cbc79336696769784abee3b307
Shiv Prakash <shivprakash@MacBook-Air.local>
2025-11-24 11:32:10 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // point to your HTML file
+    "./cards.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
